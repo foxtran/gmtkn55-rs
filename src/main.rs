4ab9cf86d7446d99f04502a378929daba2e12682
Igor S. Gerasimov <i.s.ger@ya.ru>
2025-01-17 12:50:12 +0100

    Add arguments parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::Parser;
+
+/// Evaluate datasets with given reactions
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// File with method's data
+    #[arg(long)]
+    data: std::path::PathBuf,
+
+    /// File with database
+    #[arg(long)]
+    dbfile: Option<std::path::PathBuf>,
+
+    /// DB name to compute
+    #[arg(long)]
+    compute: Option<String>,
+
+    /// Compute with uncertainty comes from reference data
+    #[arg(long, default_value_t = false)]
+    with_uncertainty: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    if args.compute.is_none() && args.dbfile.is_none() {
+        println!("Nothing to compute! Please, provide --compute or --dbfile option!");
+        return ();
+    }
+    if args.compute.is_some() {
+        panic!("NOT IMPLEMENTED");
+    }
+    if args.dbfile.is_some() {
+        panic!("NOT IMPLEMENTED");
+    }
 }
